f973f096b712844d136ac44c07200065b7022566
Christoffer2000 <chrisoffercsi@gmail.com>
2015-12-04 09:43:33 +0100

    JAJAAAAAHH view1 begun

--- semesterSeedSP/web/app/view1/view1.js
+++ semesterSeedSP/web/app/view1/view1.js
@@ -2,15 +2,31 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'app/view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view1', {
+                    templateUrl: 'view1/view1.html',
+                    controller: 'View1Ctrl',
+                    controllerAs: 'ctrl'
+                });
+            }])
 
-.controller('View1Ctrl', ["InfoFactory","InfoService",function(InfoFactory,InfoService) {
-  this.msgFromFactory = InfoFactory.getInfo();
-  this.msgFromService = InfoService.getInfo();
-}]);
+
+
+        .controller('View1Ctrl', ["$http","InfoFactory", "InfoService", function ($http, InfoFactory, InfoService) {
+                this.msgFromFactory = InfoFactory.getInfo();
+                this.msgFromService = InfoService.getInfo();
+
+                var self = this;
+                self.flights = [{"date": "2016", "airline": "Captana", "flightId":"Hej", "numberOfSeats": "4", "totalPrice": "1234kr", "traveltime": "4232", "origin": "LON"}, {"date": "2016", "airline": "Captana", "flightId":"Sut", "numberOfSeats": "4", "totalPrice": "1234kr", "traveltime": "4232min", "origin": "LON"}, {"date": "2016", "airline": "Captana", "flightId":"sovs", "numberOfSeats": "4", "totalPrice": "1234kr", "traveltime": "4232min", "origin": "LON"}];
+                self.date;
+                self.dest;
+                self.origin;
+                self.seats;
+                self.search = function(){
+                    $http({
+                        method:'Get',
+                        url: 'http://angularairline-plaul.rhcloud.com/api/flightinfo/'+self.origin+'/'+self.dest + '/' + self.date + '/' + self.seats
+                    }).then(
+                            function(data){self.flights=data.data},
+                    function(data){})};
+            }]);
